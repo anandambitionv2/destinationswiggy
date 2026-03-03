62c2361f5ef92bd6d44d774ca0b05ce9dd5cc502
anandazaugust <anandazaugust@outlook.com>
2026-03-04 04:23:31 +0530

    t

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,8 @@ app = FastAPI(title="Order API")
 # Replace with your frontend URL
 ALLOWED_ORIGINS = [
     "http://localhost:3000",              # local dev
-    "http://20.26.2.68",          # AKS LoadBalancer
+    "http://20.26.2.68",
+    "http://85.210.176.38"         # AKS LoadBalancer
     "https://yourdomain.com"              # production domain (if using ingress)
 ]
 
