f896dad50c4e25e6b335936acf2cf4bc511e2d46
anandazaugust <anandazaugust@outlook.com>
2026-04-23 04:56:55 +0530

    t

--- swiggyui/src/App.js
+++ swiggyui/src/App.js
@@ -126,7 +126,7 @@ function App() {
       <nav className="navbar">
 
         <div className="logo">
-          SWIGGY<span className="accent">ULTRAv2222</span>
+          SWIGGY<span className="accent">ULTRAv333</span>
         </div>
 
         <button
